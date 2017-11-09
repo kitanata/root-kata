e28de37c0945ce2feea221602dd17a3b7d8edc8c
Raymond Chandler III <raymondchandleriii@gmail.com>
2017-11-09 00:39:34 -0500

    Minor refactor and improvement that splits up the code a bit and sets a precendent for future commands.

--- trippy/process.py
+++ trippy/process.py
@@ -12,38 +12,38 @@ class Processor:
     def _split_line(self, line):
         return tuple(line.split(' ')) # immutable
 
+    def _process_driver_line(self, driver_name):
+        if not self.has_driver(driver_name):
+            self._drivers[driver_name] = Driver(driver_name)
+
+    def _process_trip_line(self, driver_name, start_time, end_time, miles):
+        driver = self.get_driver(driver_name)
+
+        if driver is None:
+            return
+
+        trip = Trip.parse(start_time, end_time, miles)
+
+        if trip.get_speed() > 100:
+            return
+
+        if trip.get_speed() < 5:
+            return
+
+        driver.add_trip(trip)
+
     def process_line(self, line):
         line = self._split_line(line)
         line = [item.strip() for item in line]
 
         command = line[0]
+        arguments = line[1:]
 
         if command == "Driver":
-            driver_name = line[1]
-
-            if not self.has_driver(driver_name):
-                self._drivers[driver_name] = Driver(driver_name)
+            self._process_driver_line(*arguments)
 
         elif command == "Trip":
-            driver_name = line[1]
-            start_time = line[2]
-            end_time = line[3]
-            miles = line[4]
-
-            driver = self.get_driver(driver_name)
-
-            if driver is None:
-                return
-
-            trip = Trip.parse(start_time, end_time, miles)
-
-            if trip.get_speed() > 100:
-                return
-
-            if trip.get_speed() < 5:
-                return
-
-            driver.add_trip(trip)
+            self._process_trip_line(*arguments)
 
     def has_driver(self, driver_name):
         return driver_name in self._drivers
